2f15967b7b5fcecb051b5170c1661b2bc4d27def
Enzo Porto <enzosgporto@hotmail.com>
2023-06-18 21:55:32 -0500

    Additional features and fixes

--- Assets/MonsterScript.cs
+++ Assets/MonsterScript.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class MonsterScript : MonoBehaviour
 {
 
-    private Rigidbody2D monstersBody;
+    private Rigidbody2D MonsterBody;
 
     float initialSpeed = 2f;
     Vector3 velocityZero = Vector3.zero;
     
     void Awake()
     {
-        monstersBody = GetComponent<Rigidbody2D>();
+        MonsterBody = GetComponent<Rigidbody2D>();
+
+        //creates safety check if rigidBody is not found
+        if (!MonsterBody.TryGetComponent(out Rigidbody2D playerRigidbody))
+        {
+            Debug.LogError("Rigidbody2D component not found on the PlayerMovement script's GameObject.");
+        }
     }
 
     void FixedUpdate()
     {
         float speed = initialSpeed * Time.deltaTime;
-        Vector3 targetVelocity = new Vector2(speed, 0f);
-        monstersBody.velocity = Vector3.SmoothDamp(monstersBody.velocity, targetVelocity, ref velocityZero, 0.05f);
+        Vector3 targetVelocity = new Vector2(speed, MonsterBody.velocity.y);
+        MonsterBody.velocity = Vector3.SmoothDamp(MonsterBody.velocity, targetVelocity, ref velocityZero, 0.05f);
     }
 }
